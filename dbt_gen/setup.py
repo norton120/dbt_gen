590ff472b5aacb29a5b9b7fa6fd31947ddda4816
Ethan Knox <norton120@gmail.com>
2018-02-10 10:53:17 -0500

    initial commit

---
+++ dbt_gen/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name= 'dbt_gen',
+    version='0.1',
+    description='Template generator for DBT',
+    url='https://github.com/norton120/dbt_gen',
+    author='Ethan Knox',
+    author_email='ethan.m.knox@gmail.com',
+    licence='MIT',
+    keywords='DBT data sql generator'
+    packages=['dbt_gen'],
+    install_requires=['dbt>=0.9.1',],
+    zip_safe=False)
---
+++ dbt_gen/test_dbt_gen.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+import dbt_gen
+
+
