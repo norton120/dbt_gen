c2ed07072cef8a4442f531e31075ee240c649819
Ethan Knox <norton120@gmail.com>
2018-02-24 19:21:30 -0500

    cleanup

--- dbt_gen/alooma/mid_template.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    {{ mid_template_generate('%%DBT_GEN_DATABASE%%', '%%DBT_GEN_SCHEMA%%', '%%DBT_GEN_TABLE%%',except=[%%DBT_GEN_EXCEPTIONS%%]) }}
-
-FROM
-    "%%DBT_GEN_DATABASE%%"."%%DBT_GEN_SCHEMA%%"."%%DBT_GEN_TABLE%%"
